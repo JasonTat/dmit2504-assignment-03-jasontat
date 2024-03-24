31697a0539ffd8c1f532ce1873d701b29e7d6c3a
JasonTat <112578835+JasonTat@users.noreply.github.com>
2024-03-24 13:45:33 -0600

    done

---
+++ src23/lib/services/db-service.dart
@@ -0,0 +1,117 @@
+// ignore_for_file: todo, avoid_print, library_prefixes, avoid_function_literals_in_foreach_calls, file_names, unused_import
+
+import 'package:path/path.dart' as pathPackage;
+import 'package:sqflite/sqflite.dart' as sqflitePackage;
+
+class SQFliteDbService {
+  late sqflitePackage.Database? db;
+  late String path;
+
+  Future<void> getOrCreateDatabaseHandle() async {
+    try {
+      //TODO: Put your code here to complete this method.
+      var dbPath = await sqflitePackage.getDatabasesPath();
+      path = pathPackage.join(dbPath, 'app_database.db');
+      db = await sqflitePackage.openDatabase(
+        path,
+        onCreate: (sqflitePackage.Database db1, int version) async {
+          await db1.execute( 
+            "CREATE TABLE Stocks(stockname TEXT PRIMARY KEY, symbol TEXT, price DOUBLE)",
+          );
+        },
+        version: 1,
+      );
+      print('db = $db');
+    } catch (e) {
+      print('SQFliteDbService getOrCreateDatabaseHandle: $e');
+    }
+  }
+
+  Future<void> printAllStocksInDbToConsole() async {
+    try {
+      //TODO: Put your code here to complete this method.
+      List<Map<String, dynamic>> listOfStocks = await getAllStocksFromDb();
+      if(listOfStocks.isEmpty){
+        print('No stocks in db');
+      }
+      else {
+        listOfStocks.forEach((stock) {
+          print(
+            '{stock name: ${stock['stockname']}, symbol: ${stock['symbol']}, price: \$${stock['price']}}');
+        });
+      }      
+    } catch (e) {
+      print('SQFliteDbService printAllStocksInDbToConsole: $e');
+    }
+  }
+
+  Future<List<Map<String, dynamic>>> getAllStocksFromDb() async {
+    try {
+      //TODO: Put your code here to complete this method.
+      final List<Map<String, dynamic>> listOfItems = await db!.query('Stocks');
+      return listOfItems;
+    } catch (e) {
+      print('SQFliteDbService getAllStocksFromDb: $e');
+      return <Map<String, dynamic>>[];
+    }
+  }
+
+  Future<void> deleteDb() async {
+    try {
+      //TODO: Put your code here to implement this method.
+      await sqflitePackage.deleteDatabase(path);
+      print('database deleted');
+      db = null;
+    } catch (e) {
+      print('SQFliteDbService deleteDb: $e');
+    }
+  }
+
+  Future<void> insertStock(Map<String, dynamic> stock) async {
+    try {
+      //TODO: 
+      //Put code here to insert a stock into the database.
+      //Insert the Stock into the correct table. 
+      //Also specify the conflictAlgorithm. 
+      //In this case, if the same stock is inserted
+      //multiple times, it replaces the previous data.
+      await db!.insert( 
+        'Stocks',
+        stock,
+        conflictAlgorithm: sqflitePackage.ConflictAlgorithm.replace,
+      );    
+    } catch (e) {
+      print('SQFliteDbService insertStock: $e');
+    }
+  }
+
+  Future<void> updateStock(Map<String, dynamic> stock) async {
+    try {
+      //TODO: 
+      //Put code here to update stock info.
+      await db!.update(
+        'Stocks',
+        stock,
+        where: "id = ?",
+        whereArgs: [stock['id']],
+      );
+      
+    } catch (e) {
+      print('SQFliteDbService updateStock: $e');
+    }
+  }
+
+  Future<void> deleteStock(Map<String, dynamic> stock) async {
+    try {
+      //TODO: 
+      //Put code here to delete a stock from the database.
+      await db!.delete(
+        'Stocks',
+        where: "id = ?",
+        whereArgs: [stock['id']],
+      );
+    } catch (e) {
+      print('SQFliteDbService deleteStock: $e');
+    }
+  }
+}
